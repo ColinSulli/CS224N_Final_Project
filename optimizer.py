259da7f01eaeac1d0b3e8b77421876d0ebbd4559
Abhishek Kumar <abhishek@a6k.me>
2024-05-15 07:39:09 +0530

    Add optimiser code

--- optimizer.py
+++ optimizer.py
@@ -32,18 +32,23 @@ class AdamW(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
+            for parameter in group["params"]:
+                if parameter.grad is None:
                     continue
-                grad = p.grad.data
+                grad = parameter.grad.data
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
                 # State should be stored in this dictionary.
-                state = self.state[p]
+                state = self.state[parameter]
 
                 # Access hyperparameters from the `group` dictionary.
                 alpha = group["lr"]
+                beta_1 = group['betas'][0]
+                beta_2 = group['betas'][1]
+                eps = group['eps']
+                weight_decay = group['weight_decay']
+                correct_bias = group['correct_bias']
 
                 # Complete the implementation of AdamW here, reading and saving
                 # your state in the `state` dictionary above.
@@ -59,8 +64,52 @@ class AdamW(Optimizer):
                 # 4. Apply weight decay after the main gradient-based updates.
                 # Refer to the default project handout for more details.
 
-                ### TODO
-                raise NotImplementedError
+                if not state:
+                    state = self.initialize_state()
 
+                # apply weight decay to loss
+                if loss is not None:
+                    loss = loss + weight_decay/2*torch.linalg.vector_norm(parameter)**2
+                
+                # retrive state
+                m_t_minus_1 = state['m_t_minus_1']
+                v_t_minus_1 = state['v_t_minus_1']
+                t = state['t']
+
+                # increment t
+                t += 1
+
+                # calculate m_t and v_t
+                m_t = beta_1 * m_t_minus_1 + (1 - beta_1)*grad
+                v_t = beta_2 * v_t_minus_1 + (1 - beta_2)*grad**2
+
+                # not handling the case where correct_bias is false
+                assert(correct_bias == True)
+
+                # calculate efficient bias correction
+                alpha_t = alpha * torch.sqrt(torch.tensor(1 - beta_2**t))/(1 - beta_1**t)
+
+                # update parameters
+                parameter.data = parameter.data - alpha_t * m_t / (torch.sqrt(v_t) + eps)
+
+                # subtract weight decay term from parameters
+                parameter.data = parameter.data - weight_decay*alpha*parameter.data
+
+                # persist state
+                self.persist_state(parameter, m_t, v_t, t)
 
         return loss
+    
+    def initialize_state(self):
+        return {
+            'm_t_minus_1': 0, # first moment vector
+            'v_t_minus_1': 0, # second moment vector
+            't': 0 # time step
+        }
+    
+    def persist_state(self, parameter, m_t, v_t, t):
+        self.state[parameter] = {
+            'm_t_minus_1': m_t,
+            'v_t_minus_1': v_t,
+            't': t
+        }
