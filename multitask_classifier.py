5bf334d86f3a358b721d37e1bc0cf5f6ac1b5c3b
ColinSulli <colinmi@umich.edu>
2024-06-02 14:45:32 -0700

    sts:

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -509,10 +509,11 @@ def train_multitask(rank, world_size, args):
         steps_per_epoch = 10
         probs = [0, 0, 0, 1]
     else:
-        steps_per_epoch = 600 * 3
+        steps_per_epoch = 600
         #probs = [10, 1, 1, .5]
-        probs = [283003, 8544, 1707, 6040, 8000]
+        #probs = [283003, 8544, 1707, 6040, 8000]
         #probs = [1, 1, 1, 1]
+        probs = [0, 8544, 1707, 0, 0]
 
 
 
