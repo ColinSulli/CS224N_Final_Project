bf50b2c3180639957f91afcb337c68a47da959ca
ColinSulli <colinmi@umich.edu>
2024-06-02 22:42:14 -0700

    loading crash

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -490,6 +490,7 @@ def train_multitask(rank, world_size, args):
 
     if world_size > 0:
         model = DDP(model, device_ids=[rank])
+        model = model.load_state_dict(torch.load('2024-06-03_04-33-35-full-model-10-2e-05-multitask.pt'))
 
     lr = args.lr
     optimizer = AdamW(model.parameters(), lr=lr, weight_decay=0.01)
