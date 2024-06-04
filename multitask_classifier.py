71706adf96d5859bbd99058096e70633df4cf21a
ColinSulli <colinmi@umich.edu>
2024-06-04 13:58:57 -0700

    smaller dataset

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -429,7 +429,7 @@ def train(batch, device, model, type):
     return loss
 
 def warmup_decay(current_step):
-    target_steps = 600
+    target_steps = 300
     # warmup to 1,800 steps
     if current_step < target_steps:
         return (current_step / target_steps)
